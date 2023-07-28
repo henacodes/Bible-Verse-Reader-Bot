18d975e36c2c857f47c76e4a9f709f415a7ed88c
Henok Ayenew <henacodes@gmail.com>
2023-07-28 21:41:39 +0400

    reading verse done

--- index.js
+++ index.js
@@ -1,1 +1,108 @@
-import { Bot } from "grammy";
+import { Bot, session } from "grammy";
+import dotenv from "dotenv";
+import fs from "fs";
+import {
+  readVerse,
+  initialState,
+  OtBooksKeyboard,
+  NtBooksKeyboard,
+} from "./utils.js";
+dotenv.config();
+
+const bot = new Bot(process.env.BOT_TOKEN);
+
+bot.use(session({ initial: initialState }));
+bot.command("start", (ctx) => {
+  ctx.reply(
+    "Welcome to Bible verse reader. \n To find a specific verse please send /read"
+  );
+});
+
+bot.command("read", (ctx) => {
+  ctx.reply("Choose :", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "Old Testament",
+            callback_data: "ot",
+          },
+        ],
+        [
+          {
+            text: "New Testament",
+            callback_data: "nt",
+          },
+        ],
+      ],
+    },
+  });
+});
+
+bot.callbackQuery("ot", (ctx) => {
+  ctx.session.cat = "ot";
+
+  ctx.reply("Choose from OT Books", {
+    reply_markup: {
+      inline_keyboard: OtBooksKeyboard,
+    },
+  });
+});
+
+bot.callbackQuery("nt", (ctx) => {
+  ctx.session.cat = "nt";
+
+  ctx.reply("Choose from NT Books", {
+    reply_markup: {
+      inline_keyboard: NtBooksKeyboard,
+    },
+  });
+});
+
+bot.callbackQuery("book", (ctx) => {
+  ctx.deleteMessage();
+  ctx.reply(ctx.callbackQuery);
+});
+
+bot.on("callback_query", (ctx) => {
+  const book = ctx.callbackQuery.data.split(" ")[1];
+  ctx.session.book = book;
+  ctx.reply(`Okay. what chapter do you wanna open from the book of ${book} `);
+});
+
+bot.on("message:text", (ctx) => {
+  const text = ctx.message.text;
+  const { book, chapter } = ctx.session;
+  const hasLetters = /[a-zA-Z]/.test(text);
+  if (book) {
+    if (hasLetters) {
+      ctx.reply("Send only numbers with no letter");
+    } else {
+      if (chapter) {
+        readVerse(ctx, book, chapter, parseInt(text));
+      } else {
+        ctx.session.chapter = parseInt(text);
+        ctx.reply(
+          `Now, give me the verse your wanna find from ${book} ${text} `
+        );
+      }
+    }
+  } else {
+    ctx.reply(
+      "Why did you send a text?. \n send /read if you wanna find a bible verse"
+    );
+  }
+});
+
+bot.start();
+/* 
+fs.readFile(`./data/1john.json`, "utf8", (err, data) => {
+  if (err) {
+    return ctx.reply(err.message);
+  }
+
+  // Parse the JSON data
+  const jsonData = JSON.parse(data);
+  console.log(jsonData);
+});
+ */
--- utils.js
+++ utils.js
@@ -1,16 +1,105 @@
-const fs = require("fs");
+import fs from "fs";
 
-const readVerse = (ctx, book, chapter, verse) => {
+export const initialState = () => {
+  return {
+    cat: null,
+    book: null,
+    chapter: null,
+    verse: null,
+    state: null,
+  };
+};
+
+export const readVerse = (ctx, book, chapter, verse) => {
   // Read the JSON file
   fs.readFile(`./data/${book}.json`, "utf8", (err, data) => {
     if (err) {
       return ctx.reply(err.message);
     }
-
     // Parse the JSON data
     const jsonData = JSON.parse(data);
+    if (
+      chapter > jsonData.chapters ||
+      verse > jsonData.chapters[chapter - 1].verses.length
+    ) {
+      return ctx.reply(
+        "the chapter or the verse you just gave is not available.\n Check if you gave the right chap"
+      );
+    }
     const scriptureVerse =
-      jsonData.chapters[parseInt(chapter) - 1].verses[parseInt(verse) - 1].text;
+      jsonData.chapters[chapter - 1].verses[verse - 1].text;
     return ctx.reply(scriptureVerse);
   });
 };
+
+export const NtBooksKeyboard = [
+  [{ text: "Matthew", callback_data: "book matthew" }],
+  [{ text: "Mark", callback_data: "book mark" }],
+  [{ text: "Luke", callback_data: "book luke" }],
+  [{ text: "John", callback_data: "book john" }],
+  [{ text: "Acts", callback_data: "book acts" }],
+  [{ text: "Romans", callback_data: "book romans" }],
+  [{ text: "1st Corinthians", callback_data: "book 1corinthians" }],
+  [{ text: "2nd Corinthians", callback_data: "book 2corinthians" }],
+  [{ text: "Galatians", callback_data: "book galatians" }],
+  [{ text: "Ephesians", callback_data: "book ephesians" }],
+  [{ text: "Philippians", callback_data: "book philippians" }],
+  [{ text: "Colossians", callback_data: "book colossians" }],
+  [{ text: "1st Thessalonians", callback_data: "book 1thessalonians" }],
+  [{ text: "2nd Thessalonians", callback_data: "book 2thessalonians" }],
+  [{ text: "1st Timothy", callback_data: "book 1timothy" }],
+  [{ text: "2nd Timothy", callback_data: "book 2timothy" }],
+  [{ text: "Titus", callback_data: "book titus" }],
+  [{ text: "Philemon", callback_data: "book philemon" }],
+  [{ text: "Hebrews", callback_data: "book hebrews" }],
+  [{ text: "James", callback_data: "book james" }],
+  [{ text: "1st Peter", callback_data: "book 1peter" }],
+  [{ text: "2nd Peter", callback_data: "book 2peter" }],
+  [{ text: "1st John", callback_data: "book 1john" }],
+  [{ text: "2nd John", callback_data: "book 2john" }],
+  [{ text: "3rd John", callback_data: "book 3john" }],
+  [{ text: "Jude", callback_data: "book jude" }],
+  [{ text: "Revelation", callback_data: "book revelation" }],
+];
+
+export const OtBooksKeyboard = [
+  [{ text: "Genesis", callback_data: "book genesis" }],
+  [{ text: "Exodus", callback_data: "book exodus" }],
+  [{ text: "Leviticus", callback_data: "book leviticus" }],
+  [{ text: "Numbers", callback_data: "book numbers" }],
+  [{ text: "Deuteronomy", callback_data: "book deuteronomy" }],
+  [{ text: "Joshua", callback_data: "book joshua" }],
+  [{ text: "Judges", callback_data: "book judges" }],
+  [{ text: "Ruth", callback_data: "book ruth" }],
+  [{ text: "1st Samuel", callback_data: "book 1samuel" }],
+  [{ text: "2nd Samuel", callback_data: "book 2samuel" }],
+  [{ text: "1st Kings", callback_data: "book 1kings" }],
+  [{ text: "2nd Kings", callback_data: "book 2kings" }],
+  [{ text: "1st Chronicles", callback_data: "book 1chronicles" }],
+  [{ text: "2nd Chronicles", callback_data: "book 2chronicles" }],
+  [{ text: "Ezra", callback_data: "book ezra" }],
+  [{ text: "Nehemiah", callback_data: "book nehemiah" }],
+  [{ text: "Esther", callback_data: "book esther" }],
+  [{ text: "Job", callback_data: "book job" }],
+  [{ text: "Psalms", callback_data: "book psalms" }],
+  [{ text: "Proverbs", callback_data: "book proverbs" }],
+  [{ text: "Ecclesiastes", callback_data: "book ecclesiastes" }],
+  [{ text: "Song of Solomon", callback_data: "book songofsolomon" }],
+  [{ text: "Isaiah", callback_data: "book isaiah" }],
+  [{ text: "Jeremiah", callback_data: "book jeremiah" }],
+  [{ text: "Lamentations", callback_data: "book lamentations" }],
+  [{ text: "Ezekiel", callback_data: "book ezekiel" }],
+  [{ text: "Daniel", callback_data: "book daniel" }],
+  [{ text: "Hosea", callback_data: "book hosea" }],
+  [{ text: "Joel", callback_data: "book joel" }],
+  [{ text: "Amos", callback_data: "book amos" }],
+  [{ text: "Obadiah", callback_data: "book obadiah" }],
+  [{ text: "Jonah", callback_data: "book jonah" }],
+  [{ text: "Micah", callback_data: "book micah" }],
+  [{ text: "Nahum", callback_data: "book nahum" }],
+  [{ text: "Habakkuk", callback_data: "book habakkuk" }],
+  [{ text: "Zephaniah", callback_data: "book zephaniah" }],
+  [{ text: "Haggai", callback_data: "book haggai" }],
+  [{ text: "Zechariah", callback_data: "book zechariah" }],
+  [{ text: "Malachi", callback_data: "book malachi" }],
+];
